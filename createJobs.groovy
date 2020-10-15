bcf2a33569cb42d46316c4a29352f68203ab3557
siyile <seayahh@outlook.com>
2020-10-15 16:44:19 -0700

    Add key value job

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('key-value-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/siyile/key-value.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
